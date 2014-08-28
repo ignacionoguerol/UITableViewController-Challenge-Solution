127fc74bdcf58fee8d54ce0f5d5374c2c6901f6d
ignacionoguerol <Nacho@iMac-de-Ignacio.local>
2014-08-28 12:23:34 +0200

    Problem 6 solved

---
+++ UITableViewController Challenge Solution/UITableViewController Challenge Solution/INSTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  INSTableViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Ignacio Noguerol Sicilia on 28/08/14.
+//  Copyright (c) 2014 Ignacio Noguerol Sicilia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface INSTableViewController : UITableViewController
+
+@end
